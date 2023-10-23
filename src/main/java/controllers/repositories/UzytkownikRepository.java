91ee2786a825baf24bec9798c40523c37cf8dc0b
Hei <101058402+Heixxx@users.noreply.github.com>
2023-10-23 15:58:11 +0200

    Delete src directory

--- src/main/java/controllers/repositories/UzytkownikRepository.java
+++
@@ -1,9 +0,0 @@
-package controllers.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import controllers.models.Uzytkownik;
-
-public interface UzytkownikRepository extends JpaRepository<Uzytkownik, Long>{
-  Uzytkownik findByLogin(String login);
-}
--- src/main/java/utils/MY_GLOBAL.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class MY_GLOBAL {
-
-        
-    public static String adress = "http://localhost:8081";          //W przypadku pracy lokalnej to testów   
-    
-   
-}
